b60157060e1539dd1d0bb033ef8c758eefee9a51
DanielDenenberg <dannydenenberg@gmail.com>
2018-12-23 20:49:32 -0600

    pulled

--- classifier.py
+++ classifier.py
@@ -12,7 +12,7 @@ class Perceptron():
         sum = 0
         for i in range(len(inputs)):
             sum += inputs[i] * self.weights[i]
-        return activate(sum)
+        return self.activate(sum)
 
     def train(self, inputs, target):
         #calculate error
@@ -22,23 +22,29 @@ class Perceptron():
         # update weights by adding the input times the error times the learning rate
         self.weights = [weights[i] + input[i] * error * self.learning_rate for i in range(len(input))]
 
-    def activate(n):
+    # activation function
+    def activate(self, n):
         if n >= 0:
             return 1
         else:
             return -1
 
+# simple function of x as a line
+def line(x):
+    return x
 
 # HARVARD, MIT summer courses
 
-#activation function
-def sign(n):
-    if n >= 0:
-        return 1;
-    else:
-        return -1;
+# to train the classifier, we need a set of inputs with a known answer
+class Trainer:
+    def __init__(self, x, y, ans):
+        self.inputs = [x, y, 1] # 1 is the bias or y-intercept
+        self.answer = ans
 
+data = []
+for i in range(20):
+    x = rand.randint(0, 20)
+    y = rand.randint(0, 20)
+    answer = 1 if line(x) - y < 0 else -1
 
-
-#test
-brain = Perceptron(5)
+    data.append
