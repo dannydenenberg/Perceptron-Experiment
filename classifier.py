fa11b646de43ecb30dad8b6e8847f9424a0237dd
DanielDenenberg <dannydenenberg@gmail.com>
2018-12-23 21:27:40 -0600

    completed testing

--- classifier.py
+++ classifier.py
@@ -16,11 +16,11 @@ class Perceptron():
 
     def train(self, inputs, target):
         #calculate error
-        guess = self.guess(inputs)
+        guess = self.feedforward(inputs)
         error = target - guess
 
         # update weights by adding the input times the error times the learning rate
-        self.weights = [weights[i] + input[i] * error * self.learning_rate for i in range(len(input))]
+        self.weights = [self.weights[i] + inputs[i] * error * self.learning_rate for i in range(len(inputs))]
 
     # activation function
     def activate(self, n):
@@ -31,7 +31,7 @@ class Perceptron():
 
 # simple function of x as a line
 def line(x):
-    return x
+    return x +8
 
 # HARVARD, MIT summer courses
 
@@ -41,12 +41,38 @@ class Trainer:
         self.inputs = [x, y, 1] # 1 is the bias or y-intercept
         self.answer = ans
 
+# initialize the data
 data = []
-for i in range(20):
-    x = rand.randint(0, 20)
-    y = rand.randint(0, 20)
+for i in range(2000):
+    # create random x, y pairs
+    x = rand.randint(0, 1000)
+    y = rand.randint(0, 1000)
+
+    # see if the point is above or the below the `line`
+     # by comparing the signs of the y values
     answer = 1 if line(x) - y < 0 else -1
 
     data.append(Trainer(x, y, answer))
 
-    print ("(", x, ",",y,") => ", answer)
+    #print ("(", x, ",",y,") => ", answer)
+
+
+# create the classifier
+# this is a Perceptron that takes 3 inputs (one of which is the bias) to
+    # produce a single output (-1 or 1)
+brain = Perceptron(3)
+
+
+# train the classifier (Perceptron)
+for point in data:
+    brain.train(point.inputs, point.answer)
+
+
+# testing....
+print("Have the model predict a value.")
+while True:
+    pair = [int(i) for i in input("Enter an x,y pair:").split(",")]
+    pair.append(1) # 1 for the bias
+
+    prediction = brain.feedforward(pair)
+    print ("The models prediction is ", prediction)
